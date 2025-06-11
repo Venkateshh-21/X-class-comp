da0760f27f9a882cee983a69196ca6550a0a49e2
venkatesh-21 <rajavenkatesh610@gmail.com>
2025-06-11 09:23:43 +0530

    Final commit

--- src/ClassComp.jsx
+++ src/ClassComp.jsx
@@ -22,9 +22,9 @@ class Counter extends Component{
     render(){
         return <div>
             <h1>Counter App</h1>
-            <p>Count : {this.state.count}</p>
-            <button onClick={this.increement}>Increement</button>
-            <button onClick={this.decreement}> decreeement</button>
+            <p>Count: {this.state.count}</p>
+            <button onClick={this.increement}>Increment</button>
+            <button onClick={this.decreement}>decrement</button>
         </div>
     }
 }
